6e01400c19aa5526b4b9c1e3827d19f180665c37
efraimwijaya <efraimwijaya@outlook.com>
2019-10-29 11:07:30 +0700

    added login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-<<<<<<< HEAD
-  { path: '', redirectTo: '', pathMatch: 'full' },  { path: 'home-penyedia', loadChildren: './home-penyedia/home-penyedia.module#HomePenyediaPageModule' },
-=======
-  { path: '', redirectTo: '', pathMatch: 'full' },  { path: 'home-pendaftar', loadChildren: './home-pendaftar/home-pendaftar.module#HomePendaftarPageModule' },
->>>>>>> dd71a4dc9b28ebcdd2439f4d789f2ef867875861
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: 'home-penyedia', loadChildren: './home-penyedia/home-penyedia.module#HomePenyediaPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
 
   
   ];
