f0913911a87a1568a85244347663d0f35c90710b
00Yuu <yudhistira.indrawan@gmail.com>
2019-10-29 11:14:22 +0700

    home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: 'home-penyedia', loadChildren: './home-penyedia/home-penyedia.module#HomePenyediaPageModule' },
-  { path: 'home-pendaftar', loadChildren: './home-pendaftar/home-pendaftar.module#HomePendaftarPageModule' },  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'home-pendaftar', loadChildren: './home-pendaftar/home-pendaftar.module#HomePendaftarPageModule' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },  { path: 'status-penyedia', loadChildren: './status-penyedia/status-penyedia.module#StatusPenyediaPageModule' },
+
 
 
   
