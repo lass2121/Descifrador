c064ca51e29863e27db2f0cb7dbc69032c3441c8
Neville Budiman <yeremianeville@gmail.com>
2019-10-29 11:00:54 +0700

    Create Sign Up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
-  
+  { path: 'sign-up', loadChildren: './sign-up/sign-up.module#SignUpPageModule' },
+  { path: 'sign-up-penyedia', loadChildren: './sign-up/sign-up-penyedia/sign-up-penyedia.module#SignUpPenyediaPageModule' },
+  { path: 'sign-up-user', loadChildren: './sign-up/sign-up-user/sign-up-user.module#SignUpUserPageModule' },
+
+
   ];
-  
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
