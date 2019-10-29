6cf08edb3d2b4051608c25a50080b9b9f805df27
lass2121 <akbaracaba2121@gmail.com>
2019-10-29 11:16:37 +0700

    page request, request detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: 'home-penyedia', loadChildren: './home-penyedia/home-penyedia.module#HomePenyediaPageModule' },
-  { path: 'home-pendaftar', loadChildren: './home-pendaftar/home-pendaftar.module#HomePendaftarPageModule' },  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'home-pendaftar', loadChildren: './home-pendaftar/home-pendaftar.module#HomePendaftarPageModule' },
+  { path: 'request', loadChildren: './home-penyedia/request/request.module#RequestPageModule' },
+  { path: 'detail-request', loadChildren: './home-penyedia/detail-request/detail-request.module#DetailRequestPageModule' },
 
 
   
