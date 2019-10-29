b87d3796604b4ca8900c7aa68558859c90000fda
Neville Budiman <yeremianeville@gmail.com>
2019-10-29 12:41:56 +0700

    Create Sign Up User Form

--- src/app/sign-up/sign-up-user/sign-up-user.page.ts
+++ src/app/sign-up/sign-up-user/sign-up-user.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sign-up-user',
@@ -6,10 +8,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up-user.page.scss'],
 })
 export class SignUpUserPage implements OnInit {
+  form: FormGroup;
+  validationUname: boolean;
+  validationPassword: boolean;
+  validationTypePassword: boolean;
 
-  constructor() { }
+  constructor(private alertCtrl: AlertController) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      username: new FormControl(null, {
+        updateOn: 'change',
+        validators: [Validators.required, Validators.minLength(1)]
+      }),
+      password: new FormControl(null, {
+        updateOn: 'change',
+        validators: [Validators.required, Validators.minLength(8)]
+      }),
+      reTypePassword: new FormControl(null, {
+        updateOn: 'change',
+        validators: [Validators.required, Validators.minLength(8)]
+      })
+    });
+
+    this.form.controls.username.valueChanges.subscribe(() => {
+      if (!this.form.controls.username.valid) {
+        this.validationUname = true;
+      } else {
+        this.validationUname = false;
+      }
+    });
+
+    this.form.controls.password.valueChanges.subscribe(() => {
+      if (!this.form.controls.password.valid) {
+        this.validationPassword = true;
+      } else {
+        this.validationPassword = false;
+      }
+    });
+
+    this.form.controls.reTypePassword.valueChanges.subscribe(() => {
+      if (!this.form.controls.reTypePassword.valid) {
+        this.validationTypePassword = true;
+      } else {
+        this.validationTypePassword = false;
+      }
+    });
+  }
+
+  onSubmit() {
+    // tslint:disable-next-line: max-line-length
+    if (this.validationUname !== true && this.validationPassword !== true && this.validationTypePassword) {
+      // this.offer = new Place(
+      //   this.form.value.offerId,
+      //   this.form.value.placeTitle,
+      //   this.form.value.description,
+      //   this.form.value.imgUrl,
+      //   this.form.value.price
+      // );
+      // this.offerService.editOffer(this.offer, this.detailOffer.id);
+      // this.router.navigate(['/places/tabs/offers']);
+    } else {
+      this.presentAlert();
+    }
+  }
+
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Invalid',
+      message: 'Please Complete The Form',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
 }
