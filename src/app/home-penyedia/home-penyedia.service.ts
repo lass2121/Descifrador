4ba7bbd23c5acd78c6484fb7108026e0bd1e8a38
lass2121 <akbaracaba2121@gmail.com>
2019-11-29 09:11:37 +0700

    fetch data from firebase in home-penyedia service

--- src/app/home-penyedia/home-penyedia.service.ts
+++ src/app/home-penyedia/home-penyedia.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomePenyediaService {
 
-  constructor() { }
+  request = new BehaviorSubject<Request[]>([]);
+  data : any;
+
+  constructor(private firestore: AngularFirestore) {
+
+   }
+
+   requestKegiatan() : Observable<any>{
+    return this.firestore.collection<any>("request-kegiatan").valueChanges();
+  }
+
+   readRequest() {
+    console.log(this.firestore.collection("request-kegiatan").snapshotChanges());
+    return this.firestore.collection("request-kegiatan").snapshotChanges().subscribe(data => {
+      this.data = data.map(e => {
+        return {
+          
+          nama : e.payload.doc.data()['nama']
+        };
+    });
+    for (var  i = 0; i < this.data.length; i++) {
+      
+    }
+  });;
+    
+  }
+
 }
